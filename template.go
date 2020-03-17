954f851c25d8594f00d7139665354d2c9816445d
Vadim Ipatov <vadim.ipatov@zabbix.com>
2020-03-17 11:47:04 +0200

    Partly templates support

--- template.go
+++ template.go
@@ -1,5 +1,7 @@
 package zabbix
 
+import "fmt"
+
 type Template struct {
 	TemplateID  string `json:"templateid,omitempty"`
 	Name        string `json:"host,omitempty"`
@@ -8,3 +10,43 @@ type Template struct {
 }
 
 type Templates []Template
+
+// TemplateGetParams represent the parameters for a `template.get` API call.
+//
+// See: https://www.zabbix.com/documentation/3.4/manual/api/reference/template/get#parameters
+type TemplateGetParams struct {
+	GetParameters
+	// TODO
+}
+
+// GetTemplates queries the Zabbix API for Templates matching the given search
+// parameters.
+//
+// ErrNotFound is returned if the search result set is empty.
+// An error is returned if a transport, parsing or API error occurs.
+//
+// https://www.zabbix.com/documentation/3.4/manual/api/reference/template/get
+func (c *Session) GetTemplates(params TemplateGetParams) ([]Template, error) {
+	templates := make([]jTemplate, 0)
+	err := c.Get("template.get", params, &templates)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(templates) == 0 {
+		return nil, ErrNotFound
+	}
+
+	// map JSON Events to Go Events
+	out := make([]Template, len(templates))
+	for i, jtemplate := range templates {
+		template, err := jtemplate.Template()
+		if err != nil {
+			return nil, fmt.Errorf("Error mapping Template %d in response: %v", i, err)
+		}
+
+		out[i] = *template
+	}
+
+	return out, nil
+}
--- template_json.go
+++ template_json.go
@@ -4,73 +4,46 @@ import (
 	"fmt"
 )
 
-// jHost is a private map for the Zabbix API Host object.
-// See: https://www.zabbix.com/documentation/2.2/manual/api/reference/host/object
-type jHost struct {
-	HostID         string      `json:"hostid"`
-	Hostname       string      `json:"host"`
-	Flags          int         `json:"flags,string,omitempty"`
-	Name           string      `json:"name,omitempty"`
-	Macros         []HostMacro `json:"macros,omitempty"`
-	Groups         []Hostgroup `json:"groups,omitempty"`
-	Description    string      `json:"description,omitempty"`
-	Status         int         `json:"status,string"`
-	ProxyID        string      `json:"proxy_hostid,omitempty"`
-	TLSConnect     int         `json:"tls_connect,string"`
-	TLSAccept      int         `json:"tls_accept,string"`
-	TLSIssuer      string      `json:"tls_issuer,omitempty"`
-	TLSSubject     string      `json:"tls_subject,omitempty"`
-	TLSPSKIdentity string      `json:"tls_psk_identity,omitempty"`
-	TLSPSK         string      `json:"tls_psk,omitempty"`
+// jTemplate is a private map for the Zabbix API Template object.
+// See: https://www.zabbix.com/documentation/3.4/manual/api/reference/template/object
+type jTemplate struct {
+	TemplateID  string `json:"templateid"`
+	Name        string `json:"host"`
+	Description string `json:"description,omitempty"`
+	DisplayName string `json:"name,omitempty"`
 }
 
-// Host returns a native Go Host struct mapped from the given JSON Host data.
-func (c *jHost) Host() (*Host, error) {
+// Template returns a native Go Template struct mapped from the given JSON Template data.
+func (c *jTemplate) Template() (*Template, error) {
 	//var err error
 
-	host := &Host{}
-	host.HostID = c.HostID
-	host.Hostname = c.Hostname
-	host.DisplayName = c.Name
-	host.Macros = c.Macros
-	host.Groups = c.Groups
-	host.Description   = c.Description
-	host.Status        = c.Status
-	host.ProxyID       = c.ProxyID
-	host.TLSConnect    = c.TLSConnect
-	host.TLSAccept     = c.TLSAccept
-	host.TLSIssuer     = c.TLSIssuer
-	host.TLSSubject    = c.TLSSubject
-	host.TLSPSKIdentity= c.TLSPSKIdentity
-	host.TLSPSK        = c.TLSPSK
-	/*
-		host.Source, err = strconv.Atoi(c.Flags)
-		if err != nil {
-			return nil, fmt.Errorf("Error parsing Host Flags: %v", err)
-		}
-	*/
-	host.Source = c.Flags
-	return host, nil
+	template := &Template{}
+	template.TemplateID = c.TemplateID
+	template.Name = c.Name
+	template.DisplayName = c.DisplayName
+	template.Description   = c.Description
+
+	return template, nil
 }
 
-// jHosts is a slice of jHost structs.
-type jHosts []jHost
+// jTemplates is a slice of jTemplate structs.
+type jTemplates []jTemplate
 
-// Hosts returns a native Go slice of Hosts mapped from the given JSON Hosts
+// Templates returns a native Go slice of Templates mapped from the given JSON Templates
 // data.
-func (c jHosts) Hosts() ([]Host, error) {
+func (c jTemplates) Templates() ([]Template, error) {
 	if c != nil {
-		hosts := make([]Host, len(c))
-		for i, jhost := range c {
-			host, err := jhost.Host()
+		templates := make([]Template, len(c))
+		for i, jtemplate := range c {
+			template, err := jtemplate.Template()
 			if err != nil {
-				return nil, fmt.Errorf("Error unmarshalling Host %d in JSON data: %v", i, err)
+				return nil, fmt.Errorf("Error unmarshalling Template %d in JSON data: %v", i, err)
 			}
 
-			hosts[i] = *host
+			templates[i] = *template
 		}
 
-		return hosts, nil
+		return templates, nil
 	}
 
 	return nil, nil
